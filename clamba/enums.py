aeef0526b0425924a1b6a025ae2503536f917a87
Hamdou BA <hamaba2016@gmail.com>
2025-06-12 07:34:14 +0200

    Mise en place de la stucure

--- clamba/enums.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class LLMProvider(str, Enum):
-    OLLAMA = "ollama"
-    OPENAI = "openai"
-    CLAUDE = "claude"
-    HUGGINGFACE = "huggingface"
-    FIREWORKS = "fireworks" 
